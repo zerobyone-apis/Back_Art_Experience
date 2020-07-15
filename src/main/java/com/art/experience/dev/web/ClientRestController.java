fd0591038a42ed87441ba9d9f2db1d942ea96894
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-07-14 21:15:59 -0300

    ADD LOGGERS TO CHECK OBJECT BEFORE IN TO THE LOGIC e_e

--- src/main/java/com/art/experience/dev/web/ClientRestController.java
+++ src/main/java/com/art/experience/dev/web/ClientRestController.java
@@ -3,6 +3,8 @@ package com.art.experience.dev.web;
 import com.art.experience.dev.Configuration.RestCrossOriginController;
 import com.art.experience.dev.model.Client;
 import com.art.experience.dev.service.ClientService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestCrossOriginController("/client")
 public class ClientRestController {
 
+    private static final Logger LOGGER = LogManager.getLogger(ClientRestController.class);
     private ClientService clientService;
 
     @Autowired
@@ -28,36 +31,42 @@ public class ClientRestController {
     @GetMapping("/{id_client}")
     @ResponseStatus(HttpStatus.OK)
     public Client getById(@PathVariable("id_client") final Long idClient) {
+      LOGGER.info("Email received: \n", idClient);
         return clientService.findByID(idClient);
     }
 
     @GetMapping("/email/{email}")
     @ResponseStatus(HttpStatus.OK)
     public Client getClientByEmail(@PathVariable("email") final String email) {
+      LOGGER.info("Email received: \n", email);
         return clientService.getClientByEmail(email);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Client create(@RequestBody final Client client) {
+      LOGGER.info("Email received: \n", client);
         return clientService.create(client);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Client update(@RequestBody final Client client) {
+      LOGGER.info("Object received: \n", client);
         return clientService.update(client);
     }
 
     @DeleteMapping("/logic/{id_client}")
     @ResponseStatus(HttpStatus.OK)
     public void logicDeleteByIdClient(@PathVariable("id_client") final Long client) {
+      LOGGER.info("Object received: \n", client);
         clientService.logicDelete(client);
     }
 
     @DeleteMapping("/{id_client}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteByIdClient(@PathVariable("id_client") final Long client) {
+      LOGGER.info("ID received: \n", client);
         clientService.delete(client);
     }
 
--- src/main/java/com/art/experience/dev/web/UserRestController.java
+++ src/main/java/com/art/experience/dev/web/UserRestController.java
@@ -2,7 +2,10 @@ package com.art.experience.dev.web;
 
 import com.art.experience.dev.Configuration.RestCrossOriginController;
 import com.art.experience.dev.model.User;
+import com.art.experience.dev.service.BarberService;
 import com.art.experience.dev.service.UserServices;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.Optional;
 @RestCrossOriginController("/user")
 public class UserRestController {
 
+    private static final Logger LOGGER = LogManager.getLogger(UserRestController.class);
     private UserServices userServices;
 
     @Autowired
@@ -29,24 +33,28 @@ public class UserRestController {
     @GetMapping("/{id_user}")
     @ResponseStatus(HttpStatus.OK)
     public User getById(@PathVariable("id_user") final Long idUser) {
+        LOGGER.info("Object retreived -> \n" , idUser);
         return userServices.findUsersById(idUser);
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@RequestBody final User user) {
+        LOGGER.info("Object retreived -> \n" , user);
         return userServices.createClientUser(user);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User update(@RequestBody final User user) {
+        LOGGER.info("Object retreived -> \n" , user);
         return userServices.updateUser(user);
     }
 
     @DeleteMapping("/{id_user}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteById(@PathVariable("id_user") final Long user) {
+        LOGGER.info("Object retreived -> \n" , user);
         userServices.deleteUserById(user);
     }
 
