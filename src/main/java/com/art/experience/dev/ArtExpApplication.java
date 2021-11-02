2a06e6b242b4779bf2142fb32e9194463b6cf67e
molivero <maximiliano.olivero@rappi.com>
2021-11-02 17:32:07 -0300

    War config to deploy on apache servers

--- src/main/java/com/art/experience/dev/ArtExpApplication.java
+++ src/main/java/com/art/experience/dev/ArtExpApplication.java
@@ -4,13 +4,20 @@ package com.art.experience.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class ArtExpApplication {
+public class ArtExpApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ArtExpApplication.class);
     }
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ArtExpApplication.class);
+    }
 }
